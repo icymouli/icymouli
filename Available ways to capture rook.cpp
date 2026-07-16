5ddefeb331420f9e365eae55867b566034dddfa3
icymouli <moulivishnu008@gmail.com>
2026-07-16 17:44:33 +0530

    Add files via upload

---
+++ Available ways to capture rook.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int numRookCaptures(vector<vector<char>>& board) {
+        int r, c;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == 'R') {
+                    r = i;
+                    c = j;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = r - 1; i >= 0; i--) {
+            if (board[i][c] == 'B')
+                break;
+            if (board[i][c] == 'p') {
+                count++;
+                break;
+            }
+        }
+        for (int i = r + 1; i < 8; i++) {
+            if (board[i][c] == 'B')
+                break;
+            if (board[i][c] == 'p') {
+                count++;
+                break;
+            }
+        }
+
+        for (int j = c - 1; j >= 0; j--) {
+            if (board[r][j] == 'B')
+                break;
+            if (board[r][j] == 'p') {
+                count++;
+                break;
+            }
+        }
+
+        for (int j = c + 1; j < 8; j++) {
+            if (board[r][j] == 'B')
+                break;
+            if (board[r][j] == 'p') {
+                count++;
+                break;
+            }
+        }
+
+        return count;
+    }
+};
